7f841af0f603134412eaac8bcfbf5b55c2d6a1f8
Ivo Stoyanov <ivostoy@gmail.com>
2021-02-28 11:33:07 -0800

    wip

--- scripts/setup_inception.sh
+++ scripts/setup_inception.sh
@@ -1,7 +1,3 @@
-export ASSET_DIR=./assets
-
-[[ ! -d $ASSET_DIR ]] && mkdir -p $ASSET_DIR
-
+export ASSET_DIR=./tmp/assets
+# [[ ! -d $ASSET_DIR ]] && mkdir -p $ASSET_DIR
 python3 $SNPE_ROOT/models/inception_v3/scripts/setup_inceptionv3.py --assets $ASSET_DIR --download --runtime cpu
-
-
