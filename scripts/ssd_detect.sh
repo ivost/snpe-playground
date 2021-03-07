6559380ceb5e408e264db078a3ce1526ee149992
Ivo Stoyanov <ivostoy@gmail.com>
2021-03-07 10:01:33 -0800

    images

--- scripts/ssd_detect.sh
+++ scripts/ssd_detect.sh
@@ -1,10 +1,19 @@
 SCRIPT=$(realpath "$0")
 DIR=$(dirname "$SCRIPT")
 export W=$(realpath "$DIR/../models/ssd_mobilenet_v2_coco")
-echo "$W"
+echo "DIR: $DIR"
+echo "WORKDIRL $W"
+
+rm -rf $W/processed
+echo preprocessing images
+pushd $DIR
+python ./ssd_prepare.py
+
 # snpe-dlc-info -i /home/ivo/github/snpe-playground/models/ssd_mobilenet_v2_coco/dlc/mobilenet_ssd.dlc > info.txt
 rm -rf /tmp/output
 echo running inference
-/tmp/snpe-sample -d $W/dlc/mobilenet_ssd.dlc -i $W/data/processed/file_list.txt -o /tmp/output
+/tmp/snpe-sample -d $W/dlc/mobilenet_ssd.dlc -i $W/processed/file_list.txt -o /tmp/output
 echo show results
-python $DIR/ssd_results.py -i $W/data/processed/file_list.txt -o /tmp/output -l $W/data/labels.txt
+python ./ssd_results.py -i $W/processed/file_list.txt -o /tmp/output -l $W/data/labels.txt
+
+popd
