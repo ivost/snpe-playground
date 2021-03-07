e015bbcd26e5305cd8ae94681b3931035746bf41
Ivo Stoyanov <ivostoy@gmail.com>
2021-03-07 11:29:47 -0800

    annotated

--- scripts/ssd_results.py
+++ scripts/ssd_results.py
@@ -16,7 +16,7 @@ import os
 '''
 ROOT = "/home/ivo/github/snpe-playground/models/ssd_mobilenet_v2_coco"
 LABELS = "data/labels.txt"
-FILE_LIST = "data/processed/file_list.txt"
+FILE_LIST = "processed/file_list.txt"
 RESULTS = "/tmp/output"
 
 
@@ -54,53 +54,35 @@ def main():
 
     if len(input_files) <= 0:
         print('No files listed in input_files')
-    else:
-        print('Inference results')
-        max_filename_len = max([len(file) for file in input_files])
+        return
 
-        for idx, val in enumerate(input_files):
-            output = os.path.join(output_dir, 'Result_' + str(idx))
-            num_detections = read_results(output, 'Postprocessor/BatchMultiClassNonMaxSuppression_num_detections.raw')
-            print("num_detections", num_detections)
-            n = int(num_detections[0])
+    print('Inference results')
+    max_filename_len = max([len(file) for file in input_files])
+
+    for idx, file in enumerate(input_files):
+        print("Image", file)
+        output = os.path.join(output_dir, 'Result_' + str(idx))
+        num_detections = read_results(output, 'Postprocessor/BatchMultiClassNonMaxSuppression_num_detections.raw')
+        # print("num_detections", num_detections)
+        n = int(num_detections[0])
+        if verbose:
             print("detected", n, "objects")
-            if n < 1:
-                continue
-            scores = read_results(output, 'Postprocessor/BatchMultiClassNonMaxSuppression_scores.raw')
-            scores = np.resize(scores, n)
-            print("scores", scores)
-            classes = read_results(output, 'detection_classes:0.raw')
-            classes = np.resize(classes, n)
-            print("classes", classes)
-            # same
-            # classes2 = read_results(output, 'Postprocessor/BatchMultiClassNonMaxSuppression_classes.raw')
-            # classes2 = np.resize(classes2, n)
-            # print("classes2", classes)
+        if n < 1:
+            continue
+        scores = read_results(output, 'Postprocessor/BatchMultiClassNonMaxSuppression_scores.raw')
+        scores = np.resize(scores, n)
+        print("scores", scores)
+        classes = read_results(output, 'detection_classes:0.raw')
+        classes = np.resize(classes, n)
+        print("classes", classes)
+        # same
+        # classes2 = read_results(output, 'Postprocessor/BatchMultiClassNonMaxSuppression_classes.raw')
+        # classes2 = np.resize(classes2, n)
+        # print("classes2", classes)
 
-            boxes = read_results(output, 'Postprocessor/BatchMultiClassNonMaxSuppression_boxes.raw')
-            boxes = np.resize(boxes, n*4)
-            print("boxes", boxes)
-
-            #
-            # if len(float_array) != 1001:
-            #     raise RuntimeError(str(len(float_array)) + ' outputs in ' + cur_results_file)
-
-            # if not verbose:
-            #     max_prob = max(float_array)
-            #     max_prob_index = np.where(float_array == max_prob)[0][0]
-            #     max_prob_category = labels[max_prob_index]
-            #
-            #     display_text = '%s %.2f %s %s' % (
-            #     val.ljust(max_filename_len), max_prob, str(max_prob_index).rjust(3), max_prob_category)
-            #     print(display_text)
-            # else:
-            #     top5_prob = heapq.nlargest(5, range(len(float_array)), float_array.take)
-            #     for i, idx in enumerate(top5_prob):
-            #         prob = float_array[idx]
-            #         prob_category = labels[idx]
-            #         display_text = '%s %f %s %s' % (
-            #             val.ljust(max_filename_len), prob, str(idx).rjust(3), prob_category)
-            #         print(display_text)
+        boxes = read_results(output, 'Postprocessor/BatchMultiClassNonMaxSuppression_boxes.raw')
+        boxes = np.resize(boxes, n*4)
+        print("boxes", boxes)
 
 
 def read_results(dir, name):
@@ -114,20 +96,4 @@ if __name__ == '__main__':
     main()
 
 '''
-Save tensor detection_classes:0
-Save path /tmp/output/Result_0/detection_classes:0.raw
-Saving, batchIndex 0, batchChunk 100
-Save tensor Postprocessor/BatchMultiClassNonMaxSuppression_classes
-Save path /tmp/output/Result_0/Postprocessor/BatchMultiClassNonMaxSuppression_classes.raw
-Saving, batchIndex 0, batchChunk 100
-Save tensor Postprocessor/BatchMultiClassNonMaxSuppression_scores
-Save path /tmp/output/Result_0/Postprocessor/BatchMultiClassNonMaxSuppression_scores.raw
-Saving, batchIndex 0, batchChunk 100
-Save tensor Postprocessor/BatchMultiClassNonMaxSuppression_num_detections
-Save path /tmp/output/Result_0/Postprocessor/BatchMultiClassNonMaxSuppression_num_detections.raw
-Saving, batchIndex 0, batchChunk 1
-Save tensor Postprocessor/BatchMultiClassNonMaxSuppression_boxes
-Save path /tmp/output/Result_0/Postprocessor/BatchMultiClassNonMaxSuppression_boxes.raw
-Saving, batchIndex 0, batchChunk 400
-
 '''
